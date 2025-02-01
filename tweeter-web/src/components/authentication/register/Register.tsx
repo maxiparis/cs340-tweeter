d3c598306e4ee7d61f828950f1f2b9e506a660dc
Maximiliano Paris <mparis@hotmail.cl>
2025-01-31 20:07:52 -0700

    fix: put back inputFieldGenerator in Register.tsx

--- tweeter-web/src/components/authentication/register/Register.tsx
+++ tweeter-web/src/components/authentication/register/Register.tsx
@@ -129,10 +129,59 @@ const Register = () => {
     return [user, FakeData.instance.authToken];
   };
 
+  const inputFieldGenerator = () => {
+    return (
+      <>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            size={50}
+            id="firstNameInput"
+            placeholder="First Name"
+            onKeyDown={registerOnEnter}
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+          <label htmlFor="firstNameInput">First Name</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            size={50}
+            id="lastNameInput"
+            placeholder="Last Name"
+            onKeyDown={registerOnEnter}
+            onChange={(event) => setLastName(event.target.value)}
+          />
+          <label htmlFor="lastNameInput">Last Name</label>
+        </div>
+
+        <AuthenticationFields
+          onEnter={registerOnEnter}
+          setAlias={setAlias}
+          setPassword={setPassword}
+        />
+
+        <div className="form-floating mb-3">
+          <input
+            type="file"
+            className="d-inline-block py-5 px-4 form-control bottom"
+            id="imageFileInput"
+            onKeyDown={registerOnEnter}
+            onChange={handleFileChange}
+          />
+          <label htmlFor="imageFileInput">User Image</label>
+          <img src={imageUrl} className="img-thumbnail" alt=""></img>
+        </div>
+      </>
+    );
+  };
+
   const switchAuthenticationMethodGenerator = () => {
     return (
       <div className="mb-3">
-        Algready registered? <Link to="/login">Sign in</Link>
+        Already registered? <Link to="/login">Sign in</Link>
       </div>
     );
   };
@@ -142,13 +191,7 @@ const Register = () => {
       headingText="Please Register"
       submitButtonLabel="Register"
       oAuthHeading="Register with:"
-      inputFieldGenerator={() => (
-        <AuthenticationFields
-          onEnter={registerOnEnter}
-          setAlias={setAlias}
-          setPassword={setPassword}
-        />
-      )}
+      inputFieldGenerator={inputFieldGenerator}
       switchAuthenticationMethodGenerator={switchAuthenticationMethodGenerator}
       setRememberMe={setRememberMe}
       submitButtonDisabled={checkSubmitButtonStatus}
