f283b5ba26ecaf30b917693ba54efe33d2caa79b
Maximiliano Paris <mparis@hotmail.cl>
2025-01-31 20:35:55 -0700

    feat: OAuthList with OAuthButton

---
+++ tweeter-web/src/components/authentication/OAuthList.tsx
@@ -0,0 +1,18 @@
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useToastListener from "../toaster/ToastListenerHook";
+import OAuthButton from "./OAuthButton";
+
+export default function OAuthList() {
+  return (
+    <>
+      <div className="OAuthList text-center mb-3">
+        <OAuthButton providerNameLowercase={"google"} />
+        <OAuthButton providerNameLowercase={"facebook"} />
+        <OAuthButton providerNameLowercase={"twitter"} />
+        <OAuthButton providerNameLowercase={"linkedin"} />
+        <OAuthButton providerNameLowercase={"github"} />
+      </div>
+    </>
+  );
+}
