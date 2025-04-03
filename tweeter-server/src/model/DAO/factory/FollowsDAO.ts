3340a0c6742a788f1ba81e2b7641fce70b0d597a
Maximiliano Paris <mparis@hotmail.cl>
2025-04-03 01:16:42 -0600

    feat(followsDAO): implement getFolloweesCount method

--- tweeter-server/src/model/DAO/factory/FollowsDAO.ts
+++ tweeter-server/src/model/DAO/factory/FollowsDAO.ts
@@ -1,12 +1,33 @@
-import { IUserDAO } from "../UserDAO";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 export interface IFollowsDAO {
   getFolloweesCount(alias: string): Promise<number>;
 }
 
 export class FollowsDAO implements IFollowsDAO {
-  async getFolloweesCount(alias: string): Promise<number> {
-    //TODO: implement
-    return 1997;
+  readonly tableName = "follows";
+  readonly indexName = "follows_index";
+
+  readonly followerHandleAttr = "follower_handle";
+  readonly followeeHandleAttr = "followee_handle";
+  readonly followeeNameAttr = "followee_name";
+  readonly followerNameAttr = "follower_name";
+  private readonly client = DynamoDBDocumentClient.from(new DynamoDBClient());
+
+  constructor() {}
+
+  // Returns the number of people the followerAlias follows to.
+  async getFolloweesCount(followerAlias: string): Promise<number> {
+    const params = {
+      TableName: this.tableName,
+      KeyConditionExpression: "follower_handle = :follower_handle",
+      ExpressionAttributeValues: {
+        ":follower_handle": followerAlias,
+      },
+    };
+
+    const output = await this.client.send(new QueryCommand(params));
+    return output.Items?.length || 0;
   }
 }
